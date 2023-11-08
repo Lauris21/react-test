6c63897c3efe57d8145505bbefb3a45f17e8ef16
Laura <laurasanchez0301@gmail.com>
2023-11-08 12:29:07 +0100

    button test in CounterApp

--- 01.-CounterApp-vite/tests/CounterApp.test.jsx
+++ 01.-CounterApp-vite/tests/CounterApp.test.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
 describe("Pruebas en <CounterApp />", () => {
   const value = 100;
+  const initialValue = 3;
 
   test("Debe hacer match con snapshot", () => {
     const { container } = render(<CounterApp value={value} />);
@@ -18,5 +19,26 @@ describe("Pruebas en <CounterApp />", () => {
     expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain(
       value.toString()
     );
+  });
+
+  test("Debe incrementar con el botón +1", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText("+ 1"));
+
+    expect(screen.getByText("4")).toBeTruthy();
+  });
+
+  test("Debe decrementar con el botón +1", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText("- 1"));
+
+    expect(screen.getByText("2")).toBeTruthy();
+  });
+
+  test("Debe resetear con el botón reset", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByRole("button", { name: "btn-reset" }));
+
+    expect(screen.getByText(initialValue)).toBeTruthy();
   });
 });
