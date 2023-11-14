14853a3d3d4f012a535dbc66a2b890c73fe7cea0
Laura <laurasanchez0301@gmail.com>
2023-11-14 20:32:25 +0100

    useLaoutEffect

--- Hook-App/src/03-examples/components/Figure.jsx
+++ Hook-App/src/03-examples/components/Figure.jsx
@@ -1,14 +1,36 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 export const Figure = ({ name, sprites, types }) => {
+  const h3Ref = useRef();
+
+  const [boxSize, setboxSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    console.log(h3Ref.current.getBoundingClientRect());
+    const { width, height } = h3Ref.current.getBoundingClientRect();
+
+    setboxSize({
+      width,
+      height,
+    });
+  }, []);
+
   return (
-    <figure className="figure text-center">
-      <h3>{name}</h3>
-      <img src={sprites.front_shiny} />
-      <div className="container text-center">
-        <div className="row align-items-center">
-          <p className="col">{types[0].type.name}</p>
-          <p className="col">{types[1]?.type.name}</p>
+    <>
+      <figure className="figure text-center" style={{ display: "flex" }}>
+        <h3 ref={h3Ref}>{name}</h3>
+        <img src={sprites.front_shiny} />
+        <div className="container text-center">
+          <div className="row align-items-center">
+            <p className="col">{types[0].type.name}</p>
+            <p className="col">{types[1]?.type.name}</p>
+          </div>
         </div>
-      </div>
-    </figure>
+      </figure>
+      <code>{JSON.stringify(boxSize)}</code>
+    </>
   );
 };
--- Hook-App/src/main.jsx
+++ Hook-App/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+import { Layout } from "./05-useLayoutEffect/Layout";
+//import { FocusScreen } from "./04-useRef/FocusScreen";
 //import { MultipleCustomHooks } from "./03-examples/MultipleCustomHooks";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 //import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
@@ -11,6 +12,6 @@ import { FocusScreen } from "./04-useRef/FocusScreen";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   </React.StrictMode>
 );
