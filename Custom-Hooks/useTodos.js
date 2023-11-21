420bbfc1d057d07610dd928166d9a336bd85c85c
Laura <laurasanchez0301@gmail.com>
2023-11-21 10:35:19 +0100

    custom hooks

---
+++ Custom-Hooks/useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+
+export const useTodos = () => {
+  const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+  };
+
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatchTodo(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatchTodo({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const onToggleTodo = (id) => {
+    dispatchTodo({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  const todosCount = todos.length;
+
+  const pendingTodosCount = todos.filter((item) => !item.done).length;
+
+  return {
+    todos,
+    handleNewTodo,
+    handleDeleteTodo,
+    onToggleTodo,
+    todosCount,
+    pendingTodosCount,
+  };
+};
